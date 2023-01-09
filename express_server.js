cf4a8f23f535a2204bc7eb83eb881f8265ed9400
Daniel Seniv <danielseniv@outlook.com>
2023-01-09 06:37:39 +0000

    Add route for update longURL, redirect user to /urls

--- express_server.js
+++ express_server.js
@@ -22,9 +22,9 @@ const urlDatabase = {
   
 };
 
-app.post("/urls/:id/delete", (req,res) => {
-  delete urlDatabase[req.params.id]
-  res.redirect("/urls")
+app.post("/urls/:id/update", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  res.redirect(`/urls`);
 });
 
 app.get("/", (req, res) => {
@@ -44,28 +44,30 @@ app.get("urls/new", (req,res) => {
   res.render("urls_new");
 });
 
+app.get("/urls/:id", (req,res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req,res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+app.get("/hello", (req,res) => {
+  res.send("<html><body>Hello <b>World<b></body></html>\n");
+});
+
+app.post("/urls/:id/delete", (req,res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
 
 app.post("/urls", (req, res) => {
   const id = generateRandomString();
   const longURLs = req.body.longURL;
   urlDatabase[id] = longURLs;
   res.redirect(`/urls/${id}`);
-});
-
-
-app.get("/urls/:id", (req,res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-
-
-app.get("/u/:id", (req,res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-})
-
-app.get("/hello", (req,res) => {
-  res.send("<html><body>Hello <b>World<b></body></html>\n");
 });
 
 app.listen(PORT, () => {
