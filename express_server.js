c40d1e170fd8bdbc1b3a6f8a134f0599cfe6660c
Daniel Seniv <danielseniv@outlook.com>
2023-01-14 22:41:54 +0000

    Require exported helper function from helpers.js

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cookieSession = require('cookie-session')
 const bcrypt = require('bcryptjs');
+const {getUserByEmail, generateRandomString, urlsForUser, userValidator} = require('./helpers');
 
 const app = express();
 const PORT = 8080;
@@ -16,44 +17,6 @@ app.use(cookieSession({
 }));
 
 app.set('view engine', 'ejs');
-
-
-
-const generateRandomString = function () { //function to return randomized 6 char string
-  const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  let result = "";
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-};
-
-const userValidator = function (userProperty, reqBody, database) { //Function to return false if user email registered in users database, true if not
-  for (const user in database) {
-    if (reqBody[userProperty] === database[user][userProperty]) {
-      return false
-    }
-  }
-  return true;
-}
-
-const getUserByEmail = function (email, database) {
-  for (const id in database) {
-    if (email === database[id]['email']) {
-      return database[id]['id']
-    }
-  }
-}
-
-const urlsForUser = (id, database) => {
-  const matchingIdObject = {};
-  for (const key in database) {
-    if (database[key].userID === id) {
-      matchingIdObject[key] = database[key];
-    }
-  }
-  return matchingIdObject;
-}
 
 const urlDatabase = {
 
